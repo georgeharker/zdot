6dd6e9e24cbefd183a2aeacac9841a3bc5f6b89f
George Harker <george@george-graphics.co.uk>
2026-04-27 07:34:19 +0100

    add back post-init to allow posts-configure

--- modules/autocompletion/autocompletion.zsh
+++ modules/autocompletion/autocompletion.zsh
@@ -56,6 +56,7 @@ _autocomplete_plugins_post_init() { :; }
 zdot_define_module autocomplete \
     --configure _autocomplete_plugins_configure \
     --load _autocomplete_plugins_load \
+    --post-init _autocomplete_plugins_post_init \
     --group omz-plugins \
     --requires plugins-cloned omz-bundle-initialized \
     --post-init-requires autosuggest-abbr-ready \
